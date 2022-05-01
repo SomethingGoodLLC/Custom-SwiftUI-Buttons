cb1451b264ea144abfc8d11083118544fd3e20f4
agholson <87455899+agholson@users.noreply.github.com>
2022-05-01 15:16:23 -0400

    Initial Commit

---
+++ M1L6ButtonsChallenge/M1L6ButtonsChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  M1L6ButtonsChallengeApp.swift
+//  M1L6ButtonsChallenge
+//
+//  Created by Leone on 5/1/22.
+//
+
+import SwiftUI
+
+@main
+struct M1L6ButtonsChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
